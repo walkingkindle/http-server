2a186069cb1fc4c9fe5c8d4475c3109f89be928e
Aleksa Hadzic <aki.hadza@gmail.com>
2025-03-30 18:25:42 +0200

    bug fix

--- Routes/EchoRoute.cs
+++ Routes/EchoRoute.cs
@@ -8,7 +8,7 @@ namespace codecrafters_http_server.Routes
         public override string _endpoint { get; set; } = "/echo";
         public override HttpResponse HandleRoute(HttpRequest request)
         {
-            string message = request.Endpoint.Replace(_endpoint, "").Replace("/", "");
+            string message = request.Endpoint.Replace(_endpoint, "").Replace("/", "").Trim();
             return new HttpResponse(message,"200 OK", "text/plain", Encoding.UTF8.GetByteCount(message));
 
         }
